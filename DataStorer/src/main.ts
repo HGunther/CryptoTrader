60cc2d4cbe281e722a340db1205330af233c7de2
hgunther <hnsgunther@gmail.com>
2019-11-23 00:45:03 -0700

    Added a simple message loop to DataStorer

--- DataStorer/src/main.ts
+++ DataStorer/src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import zmq = require("zeromq");
+import mongod = require("mongodb");
+
+var sock = zmq.socket("pull");
+const port = 6006;
+
+sock.connect("tcp://127.0.0.1:" + port);
+console.debug("DataStorer recieving prices on port " + port);
+
+sock.on("message", function(msg) {
+  console.log("work: %s", msg.toString());
+});
