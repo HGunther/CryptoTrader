b4bb03672ed31a20e280aef9584d6ae662c7a8a5
hgunther <hnsgunther@gmail.com>
2019-11-22 21:35:46 -0700

    Updated protobuf compiler script to output c++ and python

--- protobuf/compile.bash
+++ protobuf/compile.bash
@@ -4,10 +4,14 @@
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
  
 # Directory to write generated code to (.js and .d.ts files) 
+CPP_OUT_DIR="./cpp"
 JS_OUT_DIR="./js"
+PYTHON_OUT_DIR="./py"
  
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --cpp_out="${CPP_OUT_DIR}" \
     --js_out="import_style=commonjs,binary:${JS_OUT_DIR}" \
     --ts_out="${JS_OUT_DIR}" \
+    --python_out="${PYTHON_OUT_DIR}" \
     *.proto
