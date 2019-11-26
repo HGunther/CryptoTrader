ff3781ad870381a38013154cedfa770049a7ea3c
HGunther <hnsgunther@gmail.com>
2019-11-26 11:02:38 -0700

    Updated run scripts

--- runAll.bash
+++ runAll.bash
@@ -1,2 +1,7 @@
 #! /bin/bash
 
+# cd Broker/; ./run.bash & cd ../;
+cd DataStorer/; ./run.bash & cd ../;
+# cd Decisionmaker/; ./run.bash & cd ../;
+cd FeedSubscriber/; ./run.bash & cd ../;
+# cd Grapher/; ./run.bash & cd ../;
